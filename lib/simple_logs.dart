520c62159053e1aad6c0b65d5b0afbcd2f338da9
SabianF <58588133+SabianF@users.noreply.github.com>
2022-05-19 08:39:13 -0700

    refactor: mv class to own file

--- lib/simple_logs.dart
+++ lib/simple_logs.dart
@@ -2,6 +2,8 @@ library simple_logs;
 
 import 'dart:developer';
 
+import 'log_colour.dart';
+
 /// Colourized logs for easier readability
 class Log {
 
@@ -72,18 +74,4 @@ class Log {
     stackTrace: stackTrace ?? StackTrace.current,
     level     : 3,
   );
-}
-
-class LogColour {
-  static const String grey    = '\x1B[90m';
-  static const String red     = '\x1B[31m';
-  static const String orange  = '\x1B[33m';
-  static const String green   = '\x1B[32m';
-  static const String blue    = '\x1B[34m';
-  static const String cyan    = '\x1B[36m';
-  static const String cyan2   = '\x1B[96m';
-  static const String white   = '\x1B[37m';
-  static const String magenta = '\x1B[35m';
-  static const String magenta2= '\x1B[95m';
-  static const String end     = '\x1B[0m';
 }
